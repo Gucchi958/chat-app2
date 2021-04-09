66250a0688bd14a218c5beb6c0d535ed6c31cda0
Gucchi958 <81725643+Gucchi958@users.noreply.github.com>
2021-04-09 16:18:26 +0900

    ユーザー情報表示の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
   def edit
+  end
+
+  def update
     if current_user.update(user_params)
       redirect_to root_path 
     else
@@ -8,7 +11,7 @@ class UsersController < ApplicationController
     end
   end
 
-
+  private
   def user_params
     params.require(:user).permit(:name, :email)
   end
